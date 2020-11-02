d8b1e0492eede7c45c5770e5530bc52fdc97580c
v.huiberts <v.huiberts@quin.md>
2020-11-02 18:13:18 +0100

    Day4

--- src/main/kotlin/nl/tintie/aoc/y2019/Puzzle4.kt
+++ src/main/kotlin/nl/tintie/aoc/y2019/Puzzle4.kt
@@ -2,12 +2,40 @@ package nl.tintie.aoc.y2019
 
 import nl.tintie.aoc.AocPuzzle
 
-class Puzzle4 :AocPuzzle(2019, 4) {
+class Puzzle4 : AocPuzzle(2019, 4) {
+    private fun numberOk(number: Int, groupAssert: (List<String>) -> Boolean): Boolean {
+        val chars = number.toString()
+            .split("").drop(1).take(6)
+
+        val double = chars
+            .groupBy { it }
+            .values
+            .any(groupAssert)
+
+        val sorted = chars.sorted() == chars
+        return double && sorted
+    }
+
+
     override fun part1(): Any? {
-        TODO("Not yet implemented")
+        val (from, to) = input.first().split("-").map { it.toInt() }
+        return (from..to).count {
+            numberOk(it) {
+                it.size > 1
+            }
+        }
+    }
+
+    override fun part2(): Any? {
+        val (from, to) = input.first().split("-").map { it.toInt() }
+        return (from..to).count {
+            numberOk(it) {
+                it.size == 2
+            }
+        }
     }
 }
 
 fun main() {
-    Puzzle4().input
+    Puzzle4().runPart2()
 }
