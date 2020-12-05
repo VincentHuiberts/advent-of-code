2e14b6af640b394cb6d1d84771d00b55afd1eda1
v.huiberts <v.huiberts@quin.md>
2020-12-05 12:38:03 +0100

    Day 5

---
+++ src/main/kotlin/nl/tintie/aoc/y2020/Puzzle5.kt
@@ -0,0 +1,58 @@
+package nl.tintie.aoc.y2020
+
+import nl.tintie.aoc.AocPuzzle
+
+class Puzzle5 : AocPuzzle(2020, 5) {
+    fun getRow(boardingPass: String): Int {
+        val (lowerRange, upperRange) = boardingPass
+            .chunked(1)
+            .take(7)
+            .fold(0 to 127) { (lower, upper), region ->
+                val middle = (upper - lower) / 2 + lower
+                val roundedDown = ((upper - lower) % 2) != 0
+                when (region) {
+                    "F" -> lower to middle
+                    "B" -> (if (roundedDown) middle + 1 else middle) to upper
+                    else -> error("Unknown region: $region")
+                }
+            }
+        return if (lowerRange == upperRange) lowerRange else error("Not the same $upperRange, $lowerRange")
+    }
+
+    fun getCol(boardingPass: String): Int {
+        val (lowerRange, upperRange) = boardingPass
+            .chunked(1)
+            .drop(7)
+            .fold(0 to 7) { (lower, upper), region ->
+                val middle = (upper - lower) / 2 + lower
+                val roundedDown = ((upper - lower) % 2) != 0
+                when (region) {
+                    "L" -> lower to middle
+                    "R" -> (if (roundedDown) middle + 1 else middle) to upper
+                    else -> error("Unknown region: $region")
+                }
+            }
+        return if (lowerRange == upperRange) lowerRange else error("Not the same $upperRange, $lowerRange")
+    }
+
+    override fun part1(): Any? =
+        input.map { getRow(it) * 8 + getCol(it) }.maxOrNull()
+
+    override fun part2(): Any = input
+        .map { getCol(it) to getRow(it) }
+        .filter { (col, row) ->
+            val id = row * 8 + col
+            id == 1 || id == -1 || (row != 0 && row != 127)
+        }
+        .groupBy { (_, row) -> row }
+        .values
+        .find { it.size == 7 }!!
+        .let { seats ->
+            val missing = (0..7).find { seats.none { (col, _) -> col == it } }!!
+            seats.first().second * 8 + missing
+        }
+}
+
+fun main() {
+    Puzzle5().runBoth()
+}
