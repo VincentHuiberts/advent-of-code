2409b9489778b825a4de9201f6e96181579120e2
v.huiberts <v.huiberts@quin.md>
2020-12-05 13:42:21 +0100

    Added safety measure

--- src/main/kotlin/nl/tintie/aoc/SearchFunctions.kt
+++ src/main/kotlin/nl/tintie/aoc/SearchFunctions.kt
@@ -3,11 +3,14 @@ package nl.tintie.aoc
 fun binSearch(initLower: Long, initUpper: Long, matchFun : (Long) -> Long): Long {
     var lower = initLower
     var upper = initUpper
+    var iterations = 0L
     while (true) {
+        iterations += 1
         val current = (upper - lower) / 2 + lower
         val match = matchFun(current)
         when {
             match == 0L -> return current
+            iterations == initUpper - initLower -> error("Ran $iterations iterations, never found 0")
             match < 0 -> upper = current
             else -> lower = current
         }
