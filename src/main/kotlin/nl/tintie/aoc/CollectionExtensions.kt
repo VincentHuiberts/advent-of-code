6d698bbdf782c528311019b1f75401b7949e992c
v.huiberts <v.huiberts@quin.md>
2020-12-06 13:54:59 +0100

    Added splitCollection fun

--- src/main/kotlin/nl/tintie/aoc/CollectionExtensions.kt
+++ src/main/kotlin/nl/tintie/aoc/CollectionExtensions.kt
@@ -10,3 +10,16 @@ fun <T> Sequence<T>.combinations(n: Int): Sequence<List<T>> {
         flatMapIndexed { i, num1 -> drop(i + 1).combinations(n - 1).map { sub -> listOf(num1) + sub } }
     }
 }
+
+/**
+ * Splits a list of type `T` into multiple sublists of type `T` based on `isDelimiter`.
+ * The delimiter is not included in the result.
+ */
+fun <T> List<T>.splitCollection(isDelimiter: (T) -> Boolean): List<List<T>> =
+    fold(listOf(listOf())) { acc, element ->
+        if(isDelimiter(element)) {
+            acc.plusElement(listOf())
+        } else {
+            acc.dropLast(1).plusElement (acc.last() + element)
+        }
+    }
--- src/main/kotlin/nl/tintie/aoc/y2020/Puzzle6.kt
+++ src/main/kotlin/nl/tintie/aoc/y2020/Puzzle6.kt
@@ -1,31 +1,19 @@
 package nl.tintie.aoc.y2020
 
 import nl.tintie.aoc.AocPuzzle
+import nl.tintie.aoc.splitCollection
 
 class Puzzle6: AocPuzzle(2020, 6) {
     override fun part1(): Any? {
-        var lines = input
-        val uniqueAnswers = mutableListOf<Int>()
-        while (lines.isNotEmpty()) {
-            val group = lines.takeWhile { it.isNotBlank() }
-            lines = lines.drop(group.size + 1)
-            uniqueAnswers.add(
-                group.joinToString ("").chunked(1).filter{ it in "a".."z" }.distinct().size
-            )
+        return input.splitCollection { it.isEmpty() }.sumBy { group ->
+            group.joinToString ("").chunked(1).filter{ it in "a".."z" }.distinct().size
         }
-        return uniqueAnswers.sum()
     }
 
     override fun part2(): Any? {
-        var lines = input
-        val allSameAnswers = mutableListOf<Int>()
-        while (lines.isNotEmpty()) {
-            val group = lines.takeWhile { it.isNotBlank() }
-            lines = lines.drop(group.size + 1)
-            val allAnswered = ('a'..'z').filter { char -> group.all { person -> person.contains(char)} }.size
-            allSameAnswers.add(allAnswered)
+        return input.splitCollection { it.isEmpty() }.sumBy { group ->
+            ('a'..'z').filter { char -> group.all { person -> person.contains(char)} }.size
         }
-        return allSameAnswers.sum()
     }
 }
 
